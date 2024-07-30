baacea3c66a1b7a078b7b5ff547d6638ced1aea1
khushalrathore <118611135+khushalrathore@users.noreply.github.com>
2024-07-30 12:25:37 +0530

    init commit

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let msg = document.body.querySelector("h2#userGreeting");
 const now = new Date().getHours()
 console.log(now);
 msg.innerHTML = now > 4 && now < 12 ? "Good Morning" : (now > 12 && now < 17 ? "Good Afternoon" : "Good Evening");
+
+
+function isMobileDevice() {
+  return /Mobi|Android/i.test(navigator.userAgent);
+}
+document.addEventListener("DOMContentLoaded", function () {
+  if (isMobileDevice()) {
+    document.querySelector("body").innerHTML = "View on a larger Display<br>Mobile Support Coming Soon...";
+    document.querySelector("body").style.background = "white";
+    document.querySelector("body").style.padding = "1rem";
+  }
+});
